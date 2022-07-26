d526f8b6079a88fabae29db362e70ede56609cc7
Torstein Eliassen <32542379+gravlaks@users.noreply.github.com>
2022-07-26 21:03:52 +0200

    Changes to car driving

--- PythonClient/car/hello_car.py
+++ PythonClient/car/hello_car.py
@@ -21,67 +21,50 @@ except OSError:
     if not os.path.isdir(tmp_dir):
         raise
 
+client.startRecording()
+
 for idx in range(3):
     # get state of the car
     car_state = client.getCarState()
     print("Speed %d, Gear %d" % (car_state.speed, car_state.gear))
 
     # go forward
-    car_controls.throttle = 0.5
-    car_controls.steering = 0
-    client.setCarControls(car_controls)
-    print("Go Forward")
-    time.sleep(3)   # let car drive a bit
-
-    # Go forward + steer right
-    car_controls.throttle = 0.5
+    car_controls.throttle = 50
     car_controls.steering = 1
+    car_controls.manual_gear = 1
     client.setCarControls(car_controls)
-    print("Go Forward, steer right")
-    time.sleep(3)   # let car drive a bit
 
-    # go reverse
-    car_controls.throttle = -0.5
-    car_controls.is_manual_gear = True
-    car_controls.manual_gear = -1
-    car_controls.steering = 0
+    print("Speed %d, Gear %d" % (car_state.speed, car_state.gear))
+    time.sleep(10)   # let car drive a bit
+
+    car_controls.throttle = 50
+    car_controls.steering = 1
+    car_controls.manual_gear = 2
     client.setCarControls(car_controls)
-    print("Go reverse, steer right")
-    time.sleep(3)   # let car drive a bit
-    car_controls.is_manual_gear = False # change back gear to auto
-    car_controls.manual_gear = 0
 
-    # apply brakes
-    car_controls.brake = 1
+    print("Speed %d, Gear %d" % (car_state.speed, car_state.gear))
+    
+    time.sleep(10)   # let car drive a bit
+
+    car_controls.throttle = 50
+    car_controls.steering = 1
+    car_controls.manual_gear = 3
     client.setCarControls(car_controls)
-    print("Apply brakes")
-    time.sleep(3)   # let car drive a bit
-    car_controls.brake = 0 #remove brake
 
-    # get camera images from the car
-    responses = client.simGetImages([
-        airsim.ImageRequest("0", airsim.ImageType.DepthVis),  #depth visualization image
-        airsim.ImageRequest("1", airsim.ImageType.DepthPerspective, True), #depth in perspective projection
-        airsim.ImageRequest("1", airsim.ImageType.Scene), #scene vision image in png format
-        airsim.ImageRequest("1", airsim.ImageType.Scene, False, False)])  #scene vision image in uncompressed RGB array
-    print('Retrieved images: %d' % len(responses))
+    print("Speed %d, Gear %d" % (car_state.speed, car_state.gear))
+    time.sleep(10)   # let car drive a bit
+    
+    car_controls.throttle = 50
+    car_controls.steering = 1
+    car_controls.manual_gear = 4
+    client.setCarControls(car_controls)
 
-    for response_idx, response in enumerate(responses):
-        filename = os.path.join(tmp_dir, f"{idx}_{response.image_type}_{response_idx}")
+    print("Speed %d, Gear %d" % (car_state.speed, car_state.gear))
 
-        if response.pixels_as_float:
-            print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
-            airsim.write_pfm(os.path.normpath(filename + '.pfm'), airsim.get_pfm_array(response))
-        elif response.compress: #png format
-            print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
-            airsim.write_file(os.path.normpath(filename + '.png'), response.image_data_uint8)
-        else: #uncompressed array
-            print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
-            img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8) # get numpy array
-            img_rgb = img1d.reshape(response.height, response.width, 3) # reshape array to 3 channel image array H X W X 3
-            cv2.imwrite(os.path.normpath(filename + '.png'), img_rgb) # write to png
+    time.sleep(10)   # let car drive a bit
 
-#restore to original state
+client.stopRecording()
+
 client.reset()
 
 client.enableApiControl(False)
